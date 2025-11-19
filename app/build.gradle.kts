b516cf4621476c638d20f36587c1912d976ba1cc
tematrkv <tatarnikov.stud@mail.ru>
2025-11-19 11:50:15 +0700

    Прокинут ViewModel в UI и построен список

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,15 @@ android {
     buildFeatures {
         compose = true
     }
+
+    composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.15"
+}
 }
 
 dependencies {
 
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,6 +55,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(platform(libs.androidx.compose.bom))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -3,30 +3,52 @@ package com.example.sharedflowdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sharedflowdemo.ui.theme.SharedFlowDemoTheme
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.SharedFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            Scaffold { innerPadding ->
+                ScreenSetup(modifier = Modifier.padding(innerPadding))
             }
         }
     }
-
-@Composable
-fun ScreenSetup(modifier: Modifier = Modifier) {
-    MainScreen(modifier)
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun ScreenSetup(
+    modifier: Modifier = Modifier,
+    viewModel: DemoViewModel = viewModel()
+) {
+    MainScreen(modifier, viewModel.sharedFlow)
+}
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    sharedFlow: SharedFlow<Int>
+) {
+    val messages = remember { mutableStateListOf<Int>() }
+
+    LazyColumn(modifier = modifier) {
+        items(messages) { value ->
+            Text(
+                text = "Collected Value = $value",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(5.dp)
+            )
+        }
+    }
 }
