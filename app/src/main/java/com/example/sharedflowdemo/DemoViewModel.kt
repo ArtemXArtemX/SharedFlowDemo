a0c502dee3957fb0493fb9cab0ccad91ac1f4b43
tematrkv <tatarnikov.stud@mail.ru>
2025-11-19 11:39:46 +0700

    Добавление ViewModel в проект

---
+++ app/src/main/java/com/example/sharedflowdemo/DemoViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.sharedflowdemo
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+
+class DemoViewModel : ViewModel() {
+
+    private val _sharedFlow = MutableSharedFlow<Int>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    init {
+        sharedFlowInit()
+    }
+
+    fun sharedFlowInit() { }
+}
